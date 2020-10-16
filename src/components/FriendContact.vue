9bbf30c39941cfb1efce8b67d0ef83fc66ae4c83
Steven Calverley <steven@stevencalverley.com>
2020-10-16 18:37:29 +1100

    Add Props

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,17 +1,17 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }}</h2>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
     </button>
     <ul v-if="detailsAreVisible">
       <li>
         <strong>Phone:</strong>
-        {{ friend.phone }}
+        {{ phoneNumber }}
       </li>
       <li>
         <strong>Email:</strong>
-        {{ friend.email }}
+        {{ emailAddress }}
       </li>
     </ul>
   </li>
@@ -19,15 +19,10 @@
 
 <script>
 export default {
+  props: ["name", "phoneNumber", "emailAddress"],
   data() {
     return {
-      detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com"
-      }
+      detailsAreVisible: false
     };
   },
   methods: {
